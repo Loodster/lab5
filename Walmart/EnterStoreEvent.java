c3e6e7c12f70f4a2bac20273070ce731b776d896
magdulator <36922408+magdulator@users.noreply.github.com>
2018-03-01 13:04:27 +0100

    EnterStoreEvent.java

---
+++ Walmart/EnterStoreEvent.java
@@ -0,0 +1,46 @@
+package Walmart;
+
+import java.util.Random;
+
+import Simulator.AlreadyInQueueException;
+import Simulator.Event;
+import Simulator.EventQueue;
+import Walmart.PickUpGoodsEvent;
+import Walmart.WalmartState;
+
+public class EnterStoreEvent extends Event {
+	
+	private long startTime;
+	private WalmartState state;
+	private int customerID = 0; 
+	private static Random timeFrame = new Random(); 
+	
+	
+	public EnterStoreEvent(WalmartState state, long startTime) {
+		this.startTime = startTime;
+		this.state = state;
+	}
+
+	@Override
+	public void runEvent(EventQueue queue) {
+		if (state.isClosed() == false) {
+			if (state.isFull() == false) {    //denna kommer döpas om i state
+				customerID++; 
+				PickUpGoodsEvent nextEvent = new PickUpGoodsEvent(customerID, state, startTime + timeFrame.nextLong()); //använd håkans random annars blir han ledsen
+				queue.addEvent(nextEvent); 
+				
+			}
+			EnterStoreEvent thisEvent = new EnterStoreEvent(state, startTime + timeFrame.nextLong());
+			queue.addEvent(thisEvent);			//skapar en ny ankomsthändelse som läggs till i listan
+		}
+		
+	}
+
+	@Override
+	public long getStartTime() {
+		return startTime;
+		
+	}
+
+}
+
