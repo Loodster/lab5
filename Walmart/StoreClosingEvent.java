100c570073d8debf8b25295d4697c48eee47530e
magdulator <36922408+magdulator@users.noreply.github.com>
2018-03-03 14:14:12 +0100

    Create StoreClosingEvent.java

---
+++ Walmart/StoreClosingEvent.java
@@ -0,0 +1,31 @@
+package Walmart;
+
+import Simulator.Event;
+import Simulator.EventQueue;
+
+public class StoreClosingEvent extends Event{
+	
+	private int customerID;
+	private WalmartState state;
+	private double startTime;
+	
+	public StoreClosingEvent(int customerID, WalmartState state, double startTime) {
+		this.customerID = customerID;
+		this.state = state;
+		this.startTime = startTime;
+	}
+
+
+	@Override
+	public void runEvent(EventQueue queue) {
+		state.isClosed();
+		
+	}
+
+	@Override
+	public double getStartTime() {
+		// TODO Auto-generated method stub
+		return startTime;
+	}
+
+}	
