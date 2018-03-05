47b0ff140833e1897749a8c32f98548494ee7a49
miscobob <jonsson.r.o.michael@gmail.com>
2018-03-05 13:10:03 +0100

    Update EventQueue.java

--- EventQueue.java
+++ EventQueue.java
@@ -1,12 +1,25 @@
-package Simulator;
+package simulator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import exceptions.AlreadyInQueueException;
 
-public class EventQueue{private Node header = new Node();
+/**
+ * A class representing a queue of events,
+ * can only gain access to events when iterating over the queue
+ * @author Michael MobilaDator
+ *
+ */
+public class EventQueue implements Iterable<Event>{
+	
+	private Node header = new Node();
 	private int size = 0;
-
+	/**
+	 * Consturctor of EventQueue
+	 * @param startEvent the event that start the chain of events
+	 * @param stopEvent the event that signals the end of the chain
+	 */
 	public EventQueue(StartEvent startEvent, StopEvent stopEvent) {
 		Node start = new Node();
 		start.e = startEvent;
@@ -20,13 +33,17 @@ public class EventQueue{private Node header = new Node();
 		private Event e;
 		private Node nextNode = null;
 	}
-	public void addEvent(Event event) throws AlreadyInQueueException{
+	/**
+	 * Adds new event to queue
+	 * @param event to add to queue
+	 */
+	public void addEvent(Event event) {
 		Node node = new Node();
 		node.e = event;
 		addNode(node,header);
 	}
 	
-	private void addNode(Node node, Node compare) throws AlreadyInQueueException{
+	private void addNode(Node node, Node compare){
 		if(compare.nextNode == null) {
 			compare.nextNode = node;
 		}
@@ -44,10 +61,49 @@ public class EventQueue{private Node header = new Node();
 		}
 		}
 	}
-	public Event nextEvent() {
+	/**
+	 * Returns and removes the next event in queue will
+	 * give NullPointerException if queue is empty;
+	 * @return next event in queue
+	 */
+	private Event nextEvent() {
 		Event e = header.nextNode.e;
 		header.nextNode = header.nextNode.nextNode;
+		size--;
 		return e;
+	}
+	/**
+	 * Returns size of the event queue
+	 * @return size of queue
+	 */
+	public int getSize() {
+		return size;
+	}
+	/**
+	 * Returns a iterator of the event queue
+	 * @return iterator of the event queue
+	 */
+	@Override
+	public Iterator<Event> iterator() {
+		return new Iterator<Event>() {
+			/**
+			 * Checks if there is a next event to call
+			 * @return and checks if there a next event to call
+			 */
+			@Override
+			public boolean hasNext() {
+				return header.nextNode != null;
+			}
+			/**
+			 * Returns next event in the queue;
+			 * @return nextEvent() the next event in queue
+			 */
+			@Override
+			public Event next() {
+				return nextEvent();
+			}
+			
+		};
 	}
 	
 }
