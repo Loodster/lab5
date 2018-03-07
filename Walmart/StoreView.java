74cca481acf761c50650ddd3341af00a74e0f0ed
Loodster <36922364+Loodster@users.noreply.github.com>
2018-03-07 12:30:49 +0100

    Update StoreView.java

--- Walmart/StoreView.java
+++ Walmart/StoreView.java
@@ -6,38 +6,95 @@ import Simulator.View;
 
 public class StoreView extends View{
 	
-	public StoreView(StoreState state) {
+	private String print = "";
+	private StoreSimulator sim;
+	
+	public StoreView(StoreSimulator sim, WalmartState state) {
 		state.addObserver(this);
+		this.sim = sim;
+		
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		StoreState state = (StoreState)arg0;
-		String print = "";
+		WalmartState state = (WalmartState)arg0;
+		
 		switch(state.getEvent()) {
 		case START:
-			print = print + "PARAMETRAR" + 
-		        "\n" + "==========" + 
-			"\n" + "Antal kassor, " + "N: " + state.getCashRegisters() +
-			"\n" + "Max som ryms, M: " + state.getMax() +
-			"\n" + "Ankomsthastighet, lambda: " + "1.0" + //ändrar till parameterar
-			"\n" + "Plocktider, [P_min..Pmax]: " + "[0.5..1.0]" +//
-			"\n" + "Betaltider, [K_min..Kmax]: " + "[2.0..3.0]" + //
-			"\n" + "Frö, f: " + "1234" +//
-			"\n" +
-			"\n" + "FÖRLOPP" +
-			"\n" + "==========" +
-			"\n"+ "\t" + "Tid " + " Kund " + " ? " + " led " + "\t" + " ledT "+ "\t" +
-			" I " +" \t" + " $" + "\t" + ":-(" + "\t" + "köat" + "\t" + "köT" +
-			"\t" + "köar" + "\n" + "[Kassakö..]";
+			    print = print + "PARAMETRAR" + 
+				"\n" + "==========" + 
+				"\n" + "Antal kassor, " + "N: " + state.getCashRegisters() +
+				"\n" + "Max som ryms, M: " + sim.getMax() +
+				"\n" + "Ankomsthastighet, lambda: " + sim.getLambda() + 
+				"\n" + "Plocktider, [P_min..Pmax]: " +"[" + sim.getPickMin() + ".." + sim.getPickMax() + "]" +
+				"\n" + "Betaltider, [K_min..Kmax]: "   +"[" + sim.getPayMin() + ".." + sim.getPayMax() + "]" +  			"\n" + "Frö, f: " + state.getSeed() +//
+				"\n" +
+				"\n" + "FÖRLOPP" +
+				"\n" + "==========" +
+				"\n" + "\t" + "Tid " + " Kund " + " ? " + " led " + "\t" + " ledT "+ "\t" +
+				" I "+ " \t" + " $" + "\t" + ":-(" + "\t" + "köat" + "\t" + "köT" +
+				"\t" + "köar" + "\t" + "[Kassakö..]";
+		
+				print = print + "\t" + state.getCurrentTime() + " Start";
 			
-			
-			print = print + state.getCurrentTime();
-		case STOP:
 		case CLOSE:
+			print = print + "\t" + state.getCurrentTime() + "Stänger" +
+			"\t" + "---" +
+			"\t" + open + 
+			"\t" + state.getFreeRegisters() +
+			"\t" + state.getFreeTime() + 
+			"\t" + state.getInStore() +
+			"\t" + (state.getCustomerCount() - state.getMissedCustomer()) +
+			"\t" + state.getMissedCustomer() +
+			"\t" + //state.getAllQueued
+			"\t" + state.getQueueTime() +
+			"\t" + state.getQueueSize() +
+			"\t"; //+ state.getQueue		
+			break;
+			
+		case STOP:
+			print = print + state.getCurrentTime()+
+			"Stop" + 
+			"\n" + "\n" +
+			"RESULTAT" + "\n" +
+			"============" + "\n" +
+			"\n" + "1)" + " Av "
+			+ state.getCustomerCount() + " kunder handlade "
+			+ (state.getCustomerCount() - state.getMissedCustomer()) + " medan "
+			+ state.getMissedCustomer() + " missades." + "\n" + "\n"
+			+ "2)" + " Total tid " 
+			+ state.getCashRegisters() + " kassor varit lediga: "
+			+ state.getFreeTime() + " te."
+			+ "Genomsnitlig ledig kassatid: "
+			+ (state.getFreeTime()/state.getCashRegisters()) + " te."
+			+ "3)" + "Total tid "+ /*state.getAllQueued+ */ " kunder tvingats köa: "
+			+ state.getQueueTime() + " te." + "\n" 
+			+ "Genomsnitlig kötid: " + (state.getQueueTime()/state.getAllQueued())
+			+ " te.";
+			break;
 		default:
-			// göra mest
+			char open;
+			if(state.isClosed()) {
+				open = 's';
+			}else {
+				open = 'ö';
+			}
+			print = print + "\n" +
+			"\t" + state.getCurrentTime() + " " + state.getEvent() + 
+			"\t" + state.getCurrentCustomer() + 
+			"\t" + open + 
+			"\t" + state.getFreeRegisters() +
+			"\t" + state.getFreeTime() + 
+			"\t" + state.getInStore() +
+			"\t" + (state.getCustomerCount() - state.getMissedCustomer()) +
+			"\t" + state.getMissedCustomer() +
+			"\t" + //state.getAllQueued
+			"\t" + state.getQueueTime() +
+			"\t" + state.getQueueSize() +
+			"\t"; //+ state.getQueue
+			
+		
 		}
 		System.out.println(print);
 	}
